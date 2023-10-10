6b77026b17e9e86580542af4d04a335d4ae492db
kyoh86 <me@kyoh86.dev>
2023-10-10 18:02:15 +0900

    get commands synchronously on init

--- denops/@ddu-sources/command.ts
+++ denops/@ddu-sources/command.ts
@@ -4,7 +4,8 @@ import {
   BaseSource,
   Item,
 } from "https://deno.land/x/ddu_vim@v3.2.7/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.2.7/deps.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v3.2.7/deps.ts";
+import { OnInitArguments } from "https://deno.land/x/ddu_vim@v3.2.7/base/source.ts";
 
 export type ActionData = {
   command: string;
@@ -14,52 +15,50 @@ type Params = Record<never, never>;
 
 export class Source extends BaseSource<Params> {
   kind = "command";
-  gather(args: {
-    denops: Denops;
-    sourceParams: Params;
-  }): ReadableStream<Item<ActionData>[]> {
-    return new ReadableStream({
-      async start(controller) {
-        try {
-          /* `:command` gives lines like below.
+  commands: Item<ActionData>[] = [];
+  async onInit(_args: OnInitArguments<Params>) {
+    try {
+      /* `:command` gives lines like below.
     Name              Args Address Complete    Definition
 !   UserCommandFoo    +            command     call foo#...
 !|  UserCommandBar    *            command     call bar#...
              */
-          const commandLines = (await fn.execute(
-            args.denops,
-            "command",
-          ) as string).split("\n");
+      const commandLines = (await fn.execute(
+        _args.denops,
+        "command",
+      ) as string).split("\n");
 
-          // Header line may decides prefix size
-          commandLines.shift(); // skip first empty line
-          const header = commandLines.shift();
-          if (header === undefined) {
-            throw new Error(":command finished without output");
-          }
-          const firstColumn = /^( +)Name /.exec(header);
-          if (!firstColumn) {
-            throw new Error(":command gives unsupported format");
-          }
+      // Header line may decides prefix size
+      commandLines.shift(); // skip first empty line
+      const header = commandLines.shift();
+      if (header === undefined) {
+        throw new Error(":command finished without output");
+      }
+      const firstColumn = /^( +)Name /.exec(header);
+      if (!firstColumn) {
+        throw new Error(":command gives unsupported format");
+      }
 
-          // enqueue commands from the output.
-          const start = firstColumn[1].length;
-          controller.enqueue(
-            commandLines
-              .map((line) => /^(\w+)/.exec(line.substring(start)))
-              .filter((match): match is NonNullable<typeof match> =>
-                match != null
-              )
-              .map((match) => {
-                return {
-                  word: match[1],
-                  action: { command: match[1] },
-                };
-              }),
-          );
-        } catch (e) {
-          console.error(e);
-        }
+      // enqueue commands from the output.
+      const start = firstColumn[1].length;
+      this.commands = commandLines
+        .map((line) => /^(\w+)/.exec(line.substring(start)))
+        .filter((match): match is NonNullable<typeof match> => match != null)
+        .map((match) => {
+          return {
+            word: match[1],
+            action: { command: match[1] },
+          };
+        });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  gather(): ReadableStream<Item<ActionData>[]> {
+    const commands = this.commands;
+    return new ReadableStream({
+      pull(controller) {
+        controller.enqueue(commands);
         controller.close();
       },
     });
