8be9a630725732ba38f15a009c21716fc390f4ca
kyoh86 <me@kyoh86.dev>
2023-12-09 11:53:51 +0900

    update dependencies

--- denops/@ddu-kinds/command.ts
+++ denops/@ddu-kinds/command.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
 
 type Params = Record<never, never>;
 
--- denops/@ddu-sources/command.ts
+++ denops/@ddu-sources/command.ts
@@ -3,9 +3,9 @@ import {
   ActionFlags,
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { fn } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
-import { OnInitArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
+} from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+import { fn } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
+import { OnInitArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/source.ts";
 
 export type ActionData = {
   command: string;
