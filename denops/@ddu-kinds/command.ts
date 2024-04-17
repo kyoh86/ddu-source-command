c53f65f9258a52d1379b7079823c6ba680c66a70
kyoh86 <me@kyoh86.dev>
2024-04-17 09:25:00 +0900

    Update dependencies

--- denops/@ddu-kinds/command.ts
+++ denops/@ddu-kinds/command.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v3.10.3/types.ts";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
 
 type Params = Record<never, never>;
 
