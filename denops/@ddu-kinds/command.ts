aca947b847280715746efce59e06c96b6ac886ed
kyoh86 <me@kyoh86.dev>
2024-05-19 21:37:29 +0900

    update and fix vim version for action

--- denops/@ddu-kinds/command.ts
+++ denops/@ddu-kinds/command.ts
@@ -1,4 +1,4 @@
-import { BaseKind } from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
+import { BaseKind } from "https://deno.land/x/ddu_vim@v4.1.0/types.ts";
 
 type Params = Record<never, never>;
 
